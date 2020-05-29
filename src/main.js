42e29b1f50f82dab5f76aa9212b53ef8b1dae663
Igor Zinken <igor.zinken@igorski.nl>
2020-05-29 21:21:00 +0200

    Added initial app outline

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import store from './store'
+import Vue from 'vue';
+import VueI18n from 'vue-i18n'
+import RPG from './rpg.vue';
+import './registerServiceWorker';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.use(VueI18n);
+Vue.config.productionTip = false;
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    store,
+    render: h => h(RPG)
+}).$mount('#app');
---
+++ src/rpg.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="rpg">
+        <h1>RPG</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.rpg {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
