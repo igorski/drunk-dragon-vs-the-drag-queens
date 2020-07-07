76b90cb60e9a4af41e87f7a90597a0d09c0e9001
Igor Zinken <igor.zinken@igorski.nl>
2020-07-07 16:49:33 +0200

    Added body only version of output html

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,19 @@ const dirAssets = `${dirSrc}/assets`;
 
 module.exports = {
     lintOnSave: false,
+    publicPath: './',
+    pages: {
+        // self contained page
+        index: {
+            entry: `${dirSrc}/main.js`,
+            template: 'public/index.html',
+        },
+        // application assets only (e.g. HTML <body />)
+        app: {
+            entry: `${dirSrc}/main.js`,
+            template: 'public/index-app.html',
+        }
+    },
     configureWebpack: {
         plugins: [
             new CopyWebpackPlugin([
