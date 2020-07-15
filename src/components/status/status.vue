7f45f13c73d46e8f298883a527f7a9df4d81314f
Igor Zinken <igor.zinken@igorski.nl>
2020-07-15 20:11:37 +0200

    Styling the status window

--- src/components/status/status.vue
+++ src/components/status/status.vue
@@ -1,18 +1,21 @@
 <template>
     <modal :title="$t('status')" @close="$emit('close')">
-        <p>tja...</p>
-        <img ref="map" class="map-image" />
-        <component :is="characterComponent"
-            :character="player"
-            :width="300"
-        />
+        <div class="status-content">
+            <img ref="map" class="map-image" />
+            <component
+                class="character-preview"
+                :is="characterComponent"
+                :character="player"
+                :width="characterWidth"
+            />
+        </div>
     </modal>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import Modal          from '@/components/modal/modal';
-import { renderMap }  from '@/renderers/world-map-renderer';
+import { mapState, mapGetters } from 'vuex';
+import Modal         from '@/components/modal/modal';
+import { renderMap } from '@/renderers/world-map-renderer';
 
 import messages from './messages.json';
 
@@ -22,6 +25,9 @@ export default {
         Modal,
     },
     computed: {
+        ...mapState([
+            'dimensions',
+        ]),
         ...mapGetters([
             'activeEnvironment',
             'player',
@@ -31,9 +37,35 @@ export default {
             return () => import('@/renderers/character-female');
             //return this.player.sex === 'F' ?
         },
+        characterWidth() {
+            const ideal = 300; /* see _variables@mobile-width */
+            const { width } = this.dimensions;
+            return Math.min( ideal, width * .9 );
+        },
     },
     mounted() {
         this.$refs.map.src = renderMap( this.activeEnvironment, 1.25 ).src;
     },
 };
 </script>
+
+<style lang="scss" scoped>
+    @import '@/styles/_variables';
+
+    .status-content {
+        vertical-align: top;
+    }
+
+    .map-image {
+        width: 210px;
+        height: 210px;
+    }
+
+    @include large() {
+        .character-preview {
+            position: absolute !important;
+            right: $spacing-large;
+            top: $spacing-small;
+        }
+    }
+</style>
